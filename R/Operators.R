e1694da957022409192af19d59e5ba8d876c4bc3
edixon1 <eliotmdixon@gmail.com>
2023-01-28 11:31:34 -0500

    Implemented date operator

--- R/Operators.R
+++ R/Operators.R
@@ -28,10 +28,15 @@ mongOid <- function(id){
 #' @param field
 #' @param value
 #' TODO: document, input validation
-mongEq <- function(field, value){
+mongEq <- function(value, field){
 
+  # Check if value is numeric
   if(is.numeric(value)){
     out <- sprintf('{ "%s": %s }', field, value)
+  # Check if value is a clause
+  } else if(jsonlite::validate(value)){
+    out <- sprintf('{ "%s": %s }', field, value)
+  # Value is string
   } else {
     out <- sprintf('{ "%s": "%s" }', field, value)
   }
@@ -39,18 +44,87 @@ mongEq <- function(field, value){
   return(out)
 }
 
-#' THIS DOESN'T WORK, figure it out
 #' TODO: documentaion, input validation
-mongGte <- function(field, value){
+mongGte <- function(value, field = NULL){
 
-  out <- sprintf('{ "$gte": %s }', value)
+  if(is.null(field)){
+    out <- sprintf('{ "$gte": %s }', value)
+  } else {
+    out <- sprintf('{ "%s": { "$gte": %s } }', field, value)
+  }
+
+
 
   return(out)
 
 }
 
-# mongGte
-#
-# mongLte
-#
-# mongLt
+#' TODO: documentaion, input validation
+mongGt <- function(value, field = NULL){
+
+  if(is.null(field)){
+    out <- sprintf('{ "$gt": %s }', value)
+  } else {
+    out <- sprintf('{ "%s": { "$gt": %s } }', field, value)
+  }
+
+  return(out)
+
+}
+
+
+#' TODO: documentaion, input validation
+mongLte <- function(value, field = NULL){
+
+  if(is.null(field)){
+    out <- sprintf('{ "$lte": %s }', value)
+  } else {
+    out <- sprintf('{ "%s": { "$lte": %s } }', field, value)
+  }
+
+  return(out)
+
+}
+
+
+mongLt <- function(value, field = NULL){
+
+  if(is.null(field)){
+    out <- sprintf('{ "$lt": %s }', value)
+  } else {
+    out <- sprintf('{ "%s": { "$lt": %s } }', field, value)
+  }
+
+  return(out)
+
+}
+
+# todo: lots of testing, input validation
+#' Creates a date operator using in the BSON format ISO 8601, with a colon
+#' in the offset.
+#'@param value Date or Datetime
+#'@param tz timezone, run OlsonNames(tzdir = NULL) for a list of
+#' timezone names
+#'
+#' TODO: testing, input validation
+mongDateTime <- function(value, tz = "EST"){
+
+  timeStr <- as.POSIXlt(value, tz) |>
+    strftime("%Y-%m-%dT%H:%M:%S%z")
+
+  timeStr <- sub('(+[0-9]{2})([0-9]{2}$)','\\1:\\2', timeStr, fixed=FALSE)
+
+
+  out <- sprintf('{ "$date": "%s" }', timeStr)
+
+  return(out)
+}
+
+# todo: testing
+mongFilter <- function(clause){
+  return(sprintf('"filter": %s', clause))
+}
+
+
+
+
--- testScript.R
+++ testScript.R
@@ -2,11 +2,23 @@
 
 
 packageKey <- Sys.getenv("packageKey")
+
+
+
+
+
+
+
+
+#-----------------------Comments-------------------------------
+
+
 comments <- getCollection(url = "https://data.mongodb-api.com/app/data-nmzks/endpoint/data/beta",
                           cluster = "Cluster0",
                           database = "sample_mflix",
                           collection = "comments",
                           apiKey = packageKey)
+
 
 
 
@@ -34,3 +46,75 @@ date <- "2017-09-01"
 isoTime <- "T00:00:00.000+00:00"
 isoDateTime <- paste0(date, isoTime)
 isoDateTime
+
+
+
+#----------------------Sample Admin------------------------
+
+collection <- getCollection(url = "https://data.mongodb-api.com/app/data-nmzks/endpoint/data/beta",
+                            cluster = "Cluster0",
+                            database = "sample_admin",
+                            collection = "project_data",
+                            apiKey = packageKey)
+
+resp <- find(collection, sprintf('"filter": %s', mongLt(5000, "PNTE")))
+
+
+
+#---------------------- WeatherData----------------
+
+
+"2017-09-01"
+x <- "1997-01-26T00:00:00.000+0000"
+
+
+
+timeStr <- as.POSIXlt(Sys.time(), "America/Metlakatla") |>
+  strftime("%Y-%m-%dT%H:%M:%S%z")
+
+
+
+timeStr
+
+
+filter <- mongAnd(
+  c(
+    mongDateTime(as.Date("1997-01-26")) |>
+      mongGte() |>
+      mongEq("date"),
+    mongDateTime(as.Date("1998-01-26")) |>
+      mongLte() |>
+      mongEq("date")
+  )
+
+) |>
+  mongFilter()
+
+
+filter <- mongDateTime(as.Date("1997-01-26")) |>
+  mongGte() |>
+  mongEq("date") |>
+  mongFilter()
+
+resp <- find(comments, filter = filter)
+
+
+
+weatherCol <- getCollection(url = "https://data.mongodb-api.com/app/data-nmzks/endpoint/data/beta",
+                            cluster = "Cluster0",
+                            database = "sample_weatherdata",
+                            collection = "data",
+                            apiKey = packageKey)
+
+
+
+filter <- mongEq("_id", mongOid("5553a998e4b02cf715119412"))
+
+filter <- mongEq("")
+
+
+
+resp <- find(weatherCol, filter = sprintf('"filter": {"_id": %s}', mongOid("5553a998e4b02cf715119412")))
+
+
+
